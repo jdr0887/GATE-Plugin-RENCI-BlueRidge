2b03ba1d216c962d0ef08e1072428bc91de30d5e
Jason Reilly <jdr0887@renci.org>
2013-06-18 13:18:00 -0400

    finishing impl

--- renci-blueridge-service/src/main/java/org/renci/gate/plugin/blueridge/BlueRidgeGATEService.java
+++ renci-blueridge-service/src/main/java/org/renci/gate/plugin/blueridge/BlueRidgeGATEService.java
@@ -2,8 +2,12 @@ package org.renci.gate.plugin.blueridge;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Executors;
 
 import org.apache.commons.lang.StringUtils;
@@ -11,8 +15,11 @@ import org.renci.gate.AbstractGATEService;
 import org.renci.gate.GATEException;
 import org.renci.gate.GlideinMetric;
 import org.renci.jlrm.Queue;
+import org.renci.jlrm.pbs.PBSJobStatusInfo;
+import org.renci.jlrm.pbs.PBSJobStatusType;
 import org.renci.jlrm.pbs.ssh.PBSSSHJob;
 import org.renci.jlrm.pbs.ssh.PBSSSHKillCallable;
+import org.renci.jlrm.pbs.ssh.PBSSSHLookupStatusCallable;
 import org.renci.jlrm.pbs.ssh.PBSSSHSubmitCondorGlideinCallable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,7 +42,88 @@ public class BlueRidgeGATEService extends AbstractGATEService {
 
     @Override
     public Map<String, GlideinMetric> lookupMetrics() throws GATEException {
-        return null;
+        logger.info("ENTERING lookupMetrics()");
+        Map<String, GlideinMetric> metricsMap = new HashMap<String, GlideinMetric>();
+
+        try {
+            PBSSSHLookupStatusCallable callable = new PBSSSHLookupStatusCallable(getSite(), jobCache);
+            Set<PBSJobStatusInfo> jobStatusSet = Executors.newSingleThreadExecutor().submit(callable).get();
+            logger.debug("jobStatusSet.size(): {}", jobStatusSet.size());
+
+            // get unique list of queues
+            Set<String> queueSet = new HashSet<String>();
+            if (jobStatusSet != null && jobStatusSet.size() > 0) {
+                for (PBSJobStatusInfo info : jobStatusSet) {
+                    queueSet.add(info.getQueue());
+                }
+                for (PBSSSHJob job : jobCache) {
+                    queueSet.add(job.getQueueName());
+                }
+            }
+
+            Set<String> alreadyTalliedJobIdSet = new HashSet<String>();
+
+            if (jobStatusSet != null && jobStatusSet.size() > 0) {
+                for (PBSJobStatusInfo info : jobStatusSet) {
+                    if (metricsMap.containsKey(info.getQueue())) {
+                        continue;
+                    }
+                    if (!"glidein".equals(info.getJobName())) {
+                        continue;
+                    }
+                    metricsMap.put(info.getQueue(), new GlideinMetric(0, 0, info.getQueue()));
+                    alreadyTalliedJobIdSet.add(info.getJobId());
+                }
+
+                for (PBSJobStatusInfo info : jobStatusSet) {
+
+                    if (!"glidein".equals(info.getJobName())) {
+                        continue;
+                    }
+
+                    switch (info.getType()) {
+                        case QUEUED:
+                            metricsMap.get(info.getQueue()).incrementPending();
+                            break;
+                        case RUNNING:
+                            metricsMap.get(info.getQueue()).incrementRunning();
+                            break;
+                    }
+                }
+            }
+
+            Iterator<PBSSSHJob> jobCacheIter = jobCache.iterator();
+            while (jobCacheIter.hasNext()) {
+                PBSSSHJob nextJob = jobCacheIter.next();
+                for (PBSJobStatusInfo info : jobStatusSet) {
+
+                    if (!nextJob.getName().equals(info.getJobName())) {
+                        continue;
+                    }
+
+                    if (!alreadyTalliedJobIdSet.contains(nextJob.getId()) && nextJob.getId().equals(info.getJobId())) {
+                        switch (info.getType()) {
+                            case QUEUED:
+                                metricsMap.get(info.getQueue()).incrementPending();
+                                break;
+                            case RUNNING:
+                                metricsMap.get(info.getQueue()).incrementRunning();
+                                break;
+                            case COMPLETE:
+                                jobCacheIter.remove();
+                                break;
+                            case SUSPENDED:
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new GATEException(e);
+        }
+
+        return metricsMap;
     }
 
     @Override
@@ -57,7 +145,7 @@ public class BlueRidgeGATEService extends AbstractGATEService {
             String hostAllow = "*.unc.edu";
             PBSSSHSubmitCondorGlideinCallable callable = new PBSSSHSubmitCondorGlideinCallable();
             callable.setCollectorHost(getCollectorHost());
-            // callable.setUsername(System.getProperty("user.name"));
+            callable.setUsername(System.getProperty("user.name"));
             callable.setSite(getSite());
             callable.setJobName("glidein");
             callable.setQueue(queue);
@@ -98,20 +186,20 @@ public class BlueRidgeGATEService extends AbstractGATEService {
     @Override
     public void deletePendingGlideins() throws GATEException {
         logger.info("ENTERING deletePendingGlideins()");
-        // try {
-        // PBSSSHLookupStatusCallable lookupStatusCallable = new PBSSSHLookupStatusCallable(jobCache, getSite());
-        // Set<PBSJobStatusInfo> jobStatusSet = Executors.newSingleThreadExecutor().submit(lookupStatusCallable).get();
-        // for (PBSJobStatusInfo info : jobStatusSet) {
-        // if (info.getType().equals(PBSJobStatusType.PENDING)) {
-        // PBSSSHKillCallable killCallable = new PBSSSHKillCallable(getSite(), info.getJobId());
-        // Executors.newSingleThreadExecutor().submit(killCallable).get();
-        // }
-        // // throttle the deleteGlidein calls such that SSH doesn't complain
-        // Thread.sleep(2000);
-        // }
-        // } catch (Exception e) {
-        // throw new GATEException(e);
-        // }
+        try {
+            PBSSSHLookupStatusCallable lookupStatusCallable = new PBSSSHLookupStatusCallable(getSite(), jobCache);
+            Set<PBSJobStatusInfo> jobStatusSet = Executors.newSingleThreadExecutor().submit(lookupStatusCallable).get();
+            for (PBSJobStatusInfo info : jobStatusSet) {
+                if (info.getType().equals(PBSJobStatusType.QUEUED)) {
+                    PBSSSHKillCallable killCallable = new PBSSSHKillCallable(getSite(), info.getJobId());
+                    Executors.newSingleThreadExecutor().submit(killCallable).get();
+                }
+                // throttle the deleteGlidein calls such that SSH doesn't complain
+                Thread.sleep(2000);
+            }
+        } catch (Exception e) {
+            throw new GATEException(e);
+        }
     }
 
     public String getUsername() {
