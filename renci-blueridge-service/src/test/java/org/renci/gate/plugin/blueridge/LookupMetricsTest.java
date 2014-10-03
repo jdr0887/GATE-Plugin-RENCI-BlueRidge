a088657b6780fe1af8fed42238bee5822ace9a52
Jason Reilly <jdr0887@gmail.com>
2014-10-03 09:21:26 -0400

    fixing condition filtering by job name

--- renci-blueridge-service/src/test/java/org/renci/gate/plugin/blueridge/LookupMetricsTest.java
+++ renci-blueridge-service/src/test/java/org/renci/gate/plugin/blueridge/LookupMetricsTest.java
@@ -71,7 +71,7 @@ public class LookupMetricsTest {
                 if (metricsMap.containsKey(info.getQueue())) {
                     continue;
                 }
-                if (!"glidein".equals(info.getJobName())) {
+                if (!info.getJobName().contains("glidein")) {
                     continue;
                 }
                 metricsMap.put(info.getQueue(), new GlideinMetric("Blueridge", info.getQueue(), 0, 0));
@@ -79,7 +79,7 @@ public class LookupMetricsTest {
 
             for (PBSJobStatusInfo info : jobStatusSet) {
 
-                if (!"glidein".equals(info.getJobName())) {
+                if (!info.getJobName().contains("glidein")) {
                     continue;
                 }
 
